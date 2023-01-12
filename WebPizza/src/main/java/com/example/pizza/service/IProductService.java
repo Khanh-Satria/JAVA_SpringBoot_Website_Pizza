4f75bf468c2dfad895a15808024e4bc17b4cd781
phamtuankhanh028 <phamtuankhanh014@gmail.com>
2023-01-12 12:19:45 +0700

    update

---
+++ WebPizza/src/main/java/com/example/pizza/service/IProductService.java
@@ -0,0 +1,53 @@
+package com.example.pizza.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.example.pizza.model.Product;
+
+public interface IProductService {
+
+	void deleteAll();
+
+	void deleteAll(List<Product> entities);
+
+	void deleteAllById(List<Integer> ids);
+
+	void delete(Product entity);
+
+	void deleteById(Integer id);
+
+	long count();
+
+	
+	Page<Product> listAll(int pageNumber);
+
+	List<Product> findAllById(List<Integer> ids);
+
+	List<Product> findAll();
+
+	boolean existsById(Integer id);
+
+	Optional<Product> findById(Integer id);
+
+	List<Product> saveAll(List<Product> entities);
+
+	Page<Product> findAll(Pageable pageable);
+
+	List<Product> findAll(Sort sort);
+
+	Product save(Product entity);
+
+	List<Product> findTopSeller(int top);
+
+	Page<Product> listSortByCategory(int pageNumber, Integer idCategory);
+
+	Page<Product> listAllSort(int pageNumber, String sortText, String keyword);
+
+
+
+}
